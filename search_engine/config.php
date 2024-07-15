e8c74b2eec4b190298ba896c0211c9b66aaeae79
Masterplan <76619967+isaka-james@users.noreply.github.com>
2024-07-15 14:15:59 +0300

    remove: keep the keys on the mysql server

--- search_engine/config.php
+++ search_engine/config.php
@@ -1,10 +1,15 @@
 <?php
     // DATABASE CREDITS
     $server = "localhost";  // Replace with your database server
-    $username = "root";  // Replace with your database username
-    $password = "master123";  // Replace with your database password
     $database = "search_engine";  // Replace with your database name
 
     // connect to the database
-    $conn = mysqli_connect($server, $username, $password, $database);
+    $conn = mysqli_connect($server, $_SERVER['MYSQL_USER'],$_SERVER['MYSQL_PASSWORD'], $database);
+
+/*
+on the mysql
+SetEnv  MYSQL_USER     "susannah"
+SetEnv  MYSQL_PASSWORD "y23a!t@ce8"
+
+*/
 ?>
